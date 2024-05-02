410fc325b26763528b3102caec9545f6ffc953c1
Lawrence Adams <lawrencetadams@googlemail.com>
2024-05-02 19:16:26 +0100

    Refactor downloader.rs to improve file integrity validation

--- src/downloader.rs
+++ src/downloader.rs
@@ -35,9 +35,26 @@ pub struct Release {
     pub public_key_id: i64,
 }
 
-pub fn fetch_latest_release_metadata(release_id: u8) -> Result<Root, reqwest::Error> {
-    let todo =
-        reqwest::blocking::get(format!("https://isd.digital.nhs.uk/trud/api/v1/keys/f8b4b8e10055dfb6b34eb1fa7c114bd22db8201a/items/{release_id}/releases?latest"))?.json::<Root>()?;
+#[derive(Debug, Clone)]
+struct BadFileHash;
+
+impl std::fmt::Display for BadFileHash {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "calculated hash differed from the TRUD provided hash: file corrupted"
+        )
+    }
+}
+
+pub fn fetch_latest_release_metadata(
+    release_id: u8,
+    trud_key: String,
+) -> Result<Root, reqwest::Error> {
+    let todo = reqwest::blocking::get(format!(
+        "https://isd.digital.nhs.uk/trud/api/v1/keys/{trud_key}/items/{release_id}/releases?latest"
+    ))?
+    .json::<Root>()?;
 
     Ok(todo)
 }
@@ -45,39 +62,52 @@ pub fn fetch_latest_release_metadata(release_id: u8) -> Result<Root, reqwest::Er
 pub fn get_and_validate_file(release: &Release) -> Result<(), Box<dyn std::error::Error>> {
     let handle = reqwest::blocking::get(&release.archive_file_url)?;
     let mut _file = std::fs::File::options()
+        .create(true)
         .read(true)
         .write(true)
-        .open("./dmd.zip")?;
+        .open("./dmd.zip")
+        .expect("Unable to create file.");
 
-    _file.write_all(&handle.bytes()?)?;
+    _file
+        .write_all(&handle.bytes()?)
+        .expect("Unable to write file.");
 
-    println!("{} bytes downloaded", _file.metadata()?.len());
+    println!(
+        "  File size approx {} MB",
+        _file
+            .metadata()
+            .expect("Unable to read file metadata")
+            .len()
+            / 1024
+            / 1024
+    );
+
     println!("Ensuring file integrity maintained.");
 
+    match validate_file_hash(_file, &release.archive_file_sha256) {
+        Ok(()) => return Ok(()),
+        Err(BadFileHash) => panic!("Bad file hash. Cannot continue: {}", BadFileHash),
+    }
+}
+
+fn validate_file_hash(
+    mut file_handle: std::fs::File,
+    expected_hash: &String,
+) -> Result<(), BadFileHash> {
     let mut buffer = Vec::new();
 
-    _file
+    file_handle
         .seek(std::io::SeekFrom::Start(0))
         .expect("Unable to seek to start.");
-    _file
+    file_handle
         .read_to_end(&mut buffer)
         .expect("Unable to read to end.");
 
     let hash = sha256::digest(buffer).to_uppercase();
 
-    assert_eq!(hash, release.archive_file_sha256);
-
-    println!("  Good hash ({})", hash);
-    println!(
-        "  File size approx {} MB",
-        _file.metadata().unwrap().len() / 1024 / 1024
-    );
+    if hash != *expected_hash {
+        return Err(BadFileHash);
+    }
 
     Ok(())
-}
-
-fn validate_hash(file_handle: std::fs::File, hash: String) {
-
-
-    
 }
