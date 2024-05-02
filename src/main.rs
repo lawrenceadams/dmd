2a4ab83c87a16924ca005c3c21fb5edf07d30acd
Lawrence Adams <lawrencetadams@googlemail.com>
2024-05-02 19:32:58 +0100

    Use better error messages

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     let trud_key = env::var("TRUD_KEY").expect("No TRUD key in environment variables.");
 
-    let data = match downloader::fetch_latest_release_metadata(24, trud_key) {
+    let data = match downloader::fetch_latest_release_metadata(24, trud_key.as_str()) {
         Ok(data) => {
             println!("Got manifest ok.");
             data
@@ -14,8 +14,7 @@ fn main() {
         Err(_) => panic!("Failed to get manifest file for release_id."),
     };
 
-    match downloader::get_and_validate_file(&data.releases[0]) {
-        Ok(()) => println!("Completed okay."),
-        Err(_) => panic!("Fucked it"),
-    };
+    downloader::get_and_validate_file(&data.releases[0]).expect("Unknown error occurred.");
+
+    println!("File fetched successfully!");
 }
