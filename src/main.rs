be4c6988d58de6eb240121de06301fd9f49e359d
Lawrence Adams <lawrencetadams@googlemail.com>
2024-05-02 19:16:34 +0100

    Refactor main.rs to fetch latest release metadata using TRUD_KEY environment variable

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use std::env;
+
 mod downloader;
 
 fn main() {
     println!("Start.");
 
-    let data = match downloader::fetch_latest_release_metadata(24) {
-        Ok(data) => data,
-        Err(_) => panic!("Something gone very wrong"),
+    let trud_key = env::var("TRUD_KEY").expect("No TRUD key in environment variables.");
+
+    let data = match downloader::fetch_latest_release_metadata(24, trud_key) {
+        Ok(data) => {
+            println!("Got manifest ok.");
+            data
+        }
+        Err(_) => panic!("Failed to get manifest file for release_id."),
     };
 
     match downloader::get_and_validate_file(&data.releases[0]) {
